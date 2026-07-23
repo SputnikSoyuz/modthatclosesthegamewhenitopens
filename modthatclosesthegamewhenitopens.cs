0a05308c8ae14a10733a4cf783a7ad81f391e7c2
SputnikSoyuz <bigfatsingularity@proton.me>
2026-07-23 19:32:38 -0400

    Initial Commit

---
+++ modthatclosesthegamewhenitopens.cs
@@ -0,0 +1,20 @@
+﻿using OWML.ModHelper;
+using UnityEngine;
+
+namespace modthatclosesthegamewhenitopens
+{
+    public class modthatclosesthegamewhenitopens : ModBehaviour
+    {
+        public static modthatclosesthegamewhenitopens Instance;
+
+        public void Awake()
+        {
+            Instance = this;
+        }
+
+        public void Start()
+        {
+            Application.Quit();
+        }
+    }
+}
